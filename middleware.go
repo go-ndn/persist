fa5e42dd01eb37f5564432f9c31ca33fb79b214c
taylorchu <tailinchu@gmail.com>
2015-05-02 12:37:07 -0700

    use ndn.Sender

--- middleware.go
+++ middleware.go
@@ -6,7 +6,7 @@ import (
 )
 
 type cacher struct {
-	mux.Sender
+	ndn.Sender
 	cache ndn.Cache
 }
 
@@ -15,14 +15,14 @@ func (c *cacher) SendData(d *ndn.Data) {
 	go c.cache.Add(d)
 }
 
-func (c *cacher) Hijack() mux.Sender {
+func (c *cacher) Hijack() ndn.Sender {
 	return c.Sender
 }
 
 func Cacher(file string) mux.Middleware {
 	c, _ := New(file)
 	return func(next mux.Handler) mux.Handler {
-		return mux.HandlerFunc(func(w mux.Sender, i *ndn.Interest) {
+		return mux.HandlerFunc(func(w ndn.Sender, i *ndn.Interest) {
 			d := c.Get(i)
 			if d == nil {
 				next.ServeNDN(&cacher{Sender: w, cache: c}, i)
